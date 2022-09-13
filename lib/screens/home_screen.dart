7d5a2b57ab2e50d0e224595d0eb7d5651a465d9d
Dacosta <dacostadesarrollo@gmail.com>
2022-09-12 22:44:50 -0500

    base flutter movies

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:movies/widgets/card_swiper.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Películas en cines'),
+        elevation: 0,
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.search_off_outlined))
+        ],
+      ),
+      body: Column(children: [CardSwiper()]),
+    );
+  }
+}
